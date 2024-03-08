5f1a1678ab59a341c91256f720a6e969a1ff79e2
Aayush0815 <146852883+Aayush0815@users.noreply.github.com>
2024-03-08 17:56:35 +0530

    Cel to Fer Program

---
+++ Greenhouns/Cel to Fer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+ {
+ float f=0;
+ float c;
+ printf("Enter The Degree In Calcius:\n");
+ scanf("%f",&c);
+ f=(1.8)*c+32;
+ printf("\n The temperature in Fahrenheit: %.2f",f);
+ return 0;
+}
---
+++ Greenhouns/Third Angle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a,b,c;
+
+   printf("Enter two angles of triangle value:");
+   scanf("%d %d",&a,&b);
+
+   c=180-(a+b);
+   printf("\n Third angle of the triangle value is : %d",c);
+
+   return 0;
+}
