71d9ec0676ff72a8fc2c7787cdde5232ef429c25
Aayush0815 <146852883+Aayush0815@users.noreply.github.com>
2024-03-21 17:56:28 +0530

    Lab Work 7.2

--- Lab Work 7.2/Pattern 4.c
+++ Lab Work 7.2/Pattern 4.c
@@ -4,20 +4,17 @@ int main()
 {
     int i, j, k;
 
-    for (i = 1; i <= 5; i++)
+    for (i = 0; i < 5; i++)
     {
-        for (j = 1; j <= 5 - i; j++)
+        for (j = 0; j < i; j++)
         {
             printf("  ");
         }
-
-        for (k = i; k >= 1; k--)
+        for (k = 1; k < 6 - i; k++)
         {
-            printf("%d ", k);
+            printf("%d ", (k + i) % 2);
         }
-
         printf("\n");
     }
-
     return 0;
 }
