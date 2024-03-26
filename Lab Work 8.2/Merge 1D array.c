edfab3b2a9b9fae138698dfac6341970afab4313
Aayush0815 <146852883+Aayush0815@users.noreply.github.com>
2024-03-26 16:23:10 +0530

    Lab Work 8.2

--- Lab Work 8.2/Merge 1D array.c
+++ Lab Work 8.2/Merge 1D array.c
@@ -1,42 +1,48 @@
 #include <stdio.h>
 
-int main() {
-    int n1, n2;
+int main()
+{
+    int size_A, size_B, i;
+    printf("Enter array A size: ");
+    scanf("%d", &size_A);
 
-    printf("Enter the size of the first array: ");
-    scanf("%d", &n1);
-    int arr1[size1];
+    int array_A[size_A];
 
-    printf("Enter elements of the first array: ");
-    for (int i = 0; i < n1; i++) {
-        scanf("%d", &arr1[i]);
+    printf("Enter array A elements:\n");
+    for (i = 0; i < size_A; i++)
+    {
+        printf("a[%d] = ", i);
+        scanf("%d", &array_A[i]);
     }
 
-    printf("Enter the size of the second array: ");
-    scanf("%d", &n2);
-    int arr2[n2];
+    printf("Enter array B size: ");
+    scanf("%d", &size_B);
+    int array_B[size_B];
 
-    printf("Enter elements of the second array: ");
-    for (int i = 0; i < n2; i++) 
-        {
-        scanf("%d", &arr2[i]);
-        }
-
-    int merged[n1 + n2];
-
-    for (int i = 0; i < n1; i++) 
-        {
-        merged[i] = arr1[i];
-        }
-      for (int i = 0; i < n2; i++) 
-        {
-        merged[n1 + i] = arr2[i];
-        }
-
-    printf("Merged array: ");
-    for (int i = 0; i < n1 + n2; i++) 
+    printf("Enter array B elements:\n");
+    for (i = 0; i < size_B; i++)
     {
-        printf("%d ", merged[i]);
+        printf("b[%d] = ", i);
+        scanf("%d", &array_B[i]);
+    }
+
+    int size_C = size_A + size_B, array_C[size_C], index_C = 0;
+
+    for (i = 0; i < size_A; i++)
+    {
+        array_C[index_C++] = array_A[i];
+    }
+    for (i = 0; i < size_B; i++)
+    {
+        array_C[index_C++] = array_B[i];
+    }
+
+    printf("Array C is: ");
+    for (i = 0; i < size_C; i++)
+    {
+        printf("%d", array_C[i]);
+        if (i < size_C - 1)
+            printf(", ");
     }
     printf("\n");
 
