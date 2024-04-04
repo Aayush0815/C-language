5e0eb83f976f231bfd6f11b2bef6666cdc02a40c
Aayush0815 <146852883+Aayush0815@users.noreply.github.com>
2024-04-04 09:48:42 +0530

    Lab Work 11.2

---
+++ Indicator/String Length Using Pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int Length(char *str) {
+    int length = 0;
+    
+    while (*str != '\0') 
+    {
+        length++;
+        str++;
+    }
+    
+    return length;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+
+    int length = Length(str);
+
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
