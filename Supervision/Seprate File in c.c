28a33dc7f01eeb0745455aada196ce9300081711
Aayush0815 <146852883+Aayush0815@users.noreply.github.com>
2024-04-04 17:16:46 +0530

    Supervision

---
+++ Supervision/Seprate File in c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+  FILE *even_file = fopen("even.txt", "w");
+  FILE *odd_file = fopen("odd.txt", "w");
+
+  for (int i = 50; i <= 70; i++) 
+  {
+    if (i % 2 == 0) 
+    {
+      fprintf(even_file, "%d\n", i);
+    } else {
+      fprintf(odd_file, "%d\n", i);
+    }
+  }
+
+  fclose(even_file);
+  fclose(odd_file);
+
+  return 0;
+}
